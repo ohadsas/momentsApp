0404a9c3505ebd2c26fbbf9e4d0f36cf1ceefeef
ohadsas <ohad@panda-os.com>
2015-07-12 02:28:18 +0300

    adding map + style map Json

--- app/public/js/main.js
+++ app/public/js/main.js
@@ -11,3 +11,32 @@ function currentWindow() {
     }
   });
 };  
+
+// var json = (function () {
+//     var json = null;
+//     $.ajax({
+//         'async': true,
+//         'global': false,
+//         'url': '../json/map.json',
+//         'dataType': "json",
+//         'success': function (data) {
+//             json = data;
+//         }
+//     });
+//     return json;
+// })(); 
+
+var my_json;
+$.getJSON('../json/map.json', function(json) {
+  my_json = json;
+});
+ function initialize() {
+        var mapCanvas = document.getElementById('map-canvas');
+        var mapOptions = {
+		center: new google.maps.LatLng(50.95559560716694, -114.09205446014403),
+          zoom: 14,
+          mapTypeId: google.maps.MapTypeId.ROADMAP,
+          styles: my_json
+        }
+        var map = new google.maps.Map(mapCanvas, mapOptions);
+}
